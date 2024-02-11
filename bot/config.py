1c681dba9ad3d88332b1c9ba302af40b78339eec
songokuop <149889315+songokuop@users.noreply.github.com>
2024-02-11 07:01:14 +0530

    Fixed ✅

--- bot/config.py
+++ bot/config.py
@@ -21,7 +21,7 @@ try:
     BOT_TOKEN = config("BOT_TOKEN")
     DEV = 5467555331
     OWNER = config("OWNER")
-    ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By AnshuSharma (https://github.com/Anshusharma75/TG-videoCompress)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
+    ffmpegcode = ["-preset superfast -c:v libx265 -s 1920x1080 -x265-params 'bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 5 -metadata title='FZXAnime [t.me/FZXAnime]' -metadata author='𝖥𝖹𝖷𝖠𝗇𝗂𝗆𝖾 [รíӀҽղԵ ժҽʍօղ]' -metadata:s:a title='𝖥𝖹𝖷 𝖠𝗇𝗂𝗆𝖾 [รíӀҽղԵ ժҽʍօղ]' -metadata:s:v title='ENCODED_BY [รíӀҽղԵ ժҽʍօղ]'"]
     THUMB = config("THUMBNAIL")
 except Exception as e:
     print("Environment vars Missing")
